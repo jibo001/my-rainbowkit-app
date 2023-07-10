e3a26052087a3abd7eb49bc0eaa0cd52526b44f4
Jeanice Jones <56509810+jibo001@users.noreply.github.com>
2023-07-10 15:04:18 +0800

    env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,7 @@
+console.log('process.env');
+
+console.log(process.env);
+
 export const env = {
   chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
   projectId: 'b4915d553f2bb605acf094ff25edd1be',
