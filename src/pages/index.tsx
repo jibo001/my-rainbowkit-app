55c8d366855d4ef9ae1a8f5b739eb91ff5267262
Jeanice Jones <56509810+jibo001@users.noreply.github.com>
2023-07-10 14:34:59 +0800

    index 修改

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,10 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {
-  useAccount,
-  useChainId,
-  useContractRead,
-  useSwitchNetwork
-} from 'wagmi'
-import { useEffect, useState } from 'react'
+import { useContractRead } from 'wagmi'
 import { getIdoStakeContract } from 'utils/contractHelpers'
-import { env } from 'config/env'
 
 const Home: NextPage = () => {
-  const { address, isConnected } = useAccount()
-  const [usdt, setUsdt] = useState('')
   const idoStakeContract = getIdoStakeContract()
 
   const ido = useContractRead({
