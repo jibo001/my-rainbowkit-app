830f42c6e7365a22fbfaf67ba89aecdde2a626d0
Jeanice Jones <56509810+jibo001@users.noreply.github.com>
2023-07-10 14:52:34 +0800

    project idid

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,4 @@
 export const env = {
   chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  projectId: 'b4915d553f2bb605acf094ff25edd1be',
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { WagmiConfig, useChainId, useSwitchNetwork } from 'wagmi'
 import type { AppProps } from 'next/app'
 import { chains, wagmiConfig } from '../config/wagmi'
-import { env } from 'config/env'
-import { useEffect } from 'react'
 import { AppWrapper } from 'layout/AppWrapper'
 
 function MyApp({ Component, pageProps }: AppProps) {
